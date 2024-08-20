3ff21b3884bcc2137c2c905a94fe258eb8452683
tudhak <tudal.haccandy@gmail.com>
2024-08-20 11:02:58 +0200

    Adding topics and dynamic parameter to views.py for topics

--- src/intro/urls.py
+++ src/intro/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, topic
 
 urlpatterns = [
     path('', index, name="intro-index"),
+    path('topic<str:topic_num>/', topic, name="intro-topic"),
 ]
--- src/intro/views.py
+++ src/intro/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "intro/index.html")
+
+def topic(request, topic_num):
+    if not topic_num in ["A", "B", "C"]:
+        return render(request, "intro/topic_not_found.html")
+
+    return render(request, f"intro/topic{topic_num}.html")
