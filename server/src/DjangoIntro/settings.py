94d95ec1bd69525c4110cf5e6005e347448055c9
tudhak <tudal.haccandy@gmail.com>
2024-08-22 18:39:12 +0200

    Added django-rest-frameworks and django-cors-headers to settings.py

--- server/src/DjangoIntro/settings.py
+++ server/src/DjangoIntro/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'intro',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +52,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
+]
+
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'DjangoIntro.urls'
